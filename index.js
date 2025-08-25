8004c7f47870ea9a7af0032902b88b21ec1abcc3
rockysingh728208 <sagarkashyap9155@example.com>
2025-08-25 14:44:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(` Server is running on port ${PORT}`);
+});
