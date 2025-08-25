d3bb784c860b75534897b3ec5aeadcfd44545e0d
rockysingh728208 <sagarkashyap9155@example.com>
2025-08-25 14:55:25 +0530

    models created

---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    name: {
+         type: String,
+          required: true
+         },
+
+    email: {
+         type: String,
+         required: true,
+        unique: true
+         },
+
+    password: {
+         type: String,
+          required: true
+         },
+
+    profileImage: {
+         type: String 
+        },
+
+    isVerified: { 
+        type: Boolean,
+         default: false
+         },
+         
+    verificationCode: {
+         type: String
+         }
+
+}, { timestamps: true });
+const userModel = mongoose.model("User", userSchema);
+export default userModel;
